0b3b3fc7d55305baf3495ac6fc8f2c358e07b4d7
The Combed Thunderclap <clappedthunder@gmail.com>
2020-04-12 17:24:43 +0100

    Add terraform files and cloud build yaml

---
+++ infra/service_controls.tf
@@ -0,0 +1,58 @@
+resource "google_access_context_manager_access_policy" "platform" {
+  parent = "organizations/${var.org_id}"
+  title  = "platform-policy"
+}
+
+resource "google_access_context_manager_access_level" "default" {
+  parent = "accessPolicies/${google_access_context_manager_access_policy.platform.name}"
+  name   = "accessPolicies/${google_access_context_manager_access_policy.platform.name}/accessLevels/default"
+  title  = "default"
+  basic {
+    ip_subnetworks = [
+      "${var.inception_ip}"
+    ]
+  }
+
+  depends_on = [google_access_context_manager_access_policy.platform]
+}
+
+
+resource "google_access_context_manager_service_perimeter" "orchestration" {
+  parent = "accessPolicies/${google_access_context_manager_access_policy.access_policy.name}"
+  name   = "accessPolicies/${google_access_context_manager_access_policy.access_policy.name}/servicePerimeters/orchestration"
+  title  = "orchestration"
+  status {
+    restricted_services = local.restricted_services
+  }
+
+  depends_on = [
+    google_access_context_manager_access_policy.platform,
+    google_access_context_manager_access_level.default
+  ]
+}
+
+resource "google_access_context_manager_service_perimeter" "analytics" {
+  parent = "accessPolicies/${google_access_context_manager_access_policy.access_policy.name}"
+  name   = "accessPolicies/${google_access_context_manager_access_policy.access_policy.name}/servicePerimeters/analytics"
+  title  = "analytics"
+  status {
+    restricted_services = local.restricted_services
+  }
+
+  depends_on = [
+    google_access_context_manager_access_policy.platform,
+    google_access_context_manager_access_level.default
+  ]
+}
+
+resource "google_access_context_manager_service_perimeter" "orchestration_analytics_bridge" {
+  parent         = "accessPolicies/${google_access_context_manager_access_policy.access_policy.name}"
+  name           = "accessPolicies/${google_access_context_manager_access_policy.access_policy.name}/servicePerimeters/orchestration-analytics-bridge"
+  title          = "orchestration-analytics-bridge"
+  perimeter_type = "PERIMETER_TYPE_BRIDGE"
+
+  depends_on = [
+    google_access_context_manager_service_perimeter.orchestration,
+    google_access_context_manager_service_perimeter.analytics,
+  ]
+}
